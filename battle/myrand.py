b0aad9cf14ef6e5023496962c98235d80ec24c38
Oscar Lin <osclin22@gmail.com>
2017-03-08 19:45:39 +0800

    remove stupidity

--- battle/myrand.py
+++
@@ -1,8 +0,0 @@
-import random as rnd
-
-
-def random_list(list_len):
-    list_of_random_numbers = []
-    for _ in range(list_len):
-        list_of_random_numbers.append(rnd.random())
-    return list_of_random_numbers
