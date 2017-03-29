46f1a0aefd63f38d9e34bcc88a09a8e3c9087761
Oscar Lin <osclin22@gmail.com>
2017-03-29 19:55:46 +0800

    your homework awaits.  AND I WANT TESTS

--- battle/units.py
+++ battle/units.py
@@ -6,10 +6,15 @@ FIST = Weapon(1, 1)
 
 class Soldier(BaseUnit):
     def __init__(self):
-        self._health = 100
-        self._move = 3
+        self._max_health = 100
+        self._max_move = 3
+        self._current_health = self._max_health
+        self._current_move = self._max_move
         self._weapon = FIST
         super(Soldier, self).__init__()
+
+    def get_health(self) -> int:
+        return self._current_health
 
     def get_weapon(self) -> Weapon:
         return self._weapon
@@ -22,13 +27,38 @@ class Soldier(BaseUnit):
         opponent.receive_dmg(dmg)
 
     def is_dead(self) -> bool:
-        return self._health <= 0
+        return self._current_health <= 0
 
     def receive_dmg(self, dmg: int):
-        self._health -= dmg
+        if dmg < 0:
+            raise ValueError('damage can\'t be less than zero')
+        self._current_health -= dmg
 
-    def get_health(self) -> int:
-        return self._health
+    def heal(self, health_pts: int):
+        """cannot heal past max"""
+        raise NotImplementedError
+
+    def move(self, move_pts: int):
+        """cannot move if currentt is max"""
+        raise NotImplementedError
+
+    def can_move(self) -> bool:
+        raise NotImplementedError
+    
+    def reset_move(self):
+        raise NotImplementedError
+
+    def rest(self):
+        """heal and reset_move"""
+        raise NotImplementedError
+
+
+
+
+class Example(Soldier):
+    def __init__(self, weapon: Weapon):
+        super(Example, self).__init__()
+        self.equip_weapon(weapon)
 
 
 
--- tests/test_units.py
+++ tests/test_units.py
@@ -8,11 +8,9 @@ class TestSoldier(TestBaseUnit):
     def setUp(self):
         self.unit = Soldier()
 
-    def test_weapon_is_fist(self):
-        """
-                bad test.  cannot access private properties
-                """
+    def test_init_default_settings(self):
         self.assertIs(self.unit.get_weapon(), FIST)
+        self.assertEqual(self.unit.get_health(), 100)
 
     def test_equip_weapon(self):
         stick = Weapon(3, 2)
@@ -21,6 +19,52 @@ class TestSoldier(TestBaseUnit):
 
     def test_receive_dmg(self):
         self.unit.receive_dmg(99)
+        self.assertEqual(self.unit.get_health(), 1)
+
+        self.unit.receive_dmg(2)
+        self.assertEqual(self.unit.get_health(), -1)
+
+        self.unit.receive_dmg(0)
+        self.assertEqual(self.unit.get_health(), -1)
+
+    def test_receive_dmg_negative_number_raises_value_error(self):
+        self.assertRaises(ValueError, self.unit.receive_dmg, -10)
+
+    def test_attack(self):
+        opponent = Soldier()
+        self.assertEqual(self.unit.get_weapon().atk_dmg, 1)
+
+        self.unit.attack(opponent)
+        self.assertEqual(opponent.get_health(), 99)
+
+        self.unit.attack(opponent)
+        self.assertEqual(opponent.get_health(), 98)
+
+    def test_attack_with_different_weapon(self):
+        opponent = Soldier()
+        self.unit.equip_weapon(Weapon(10, 1))
+        self.assertEqual(self.unit.get_weapon().atk_dmg, 10)
+
+        self.unit.attack(opponent)
+        self.assertEqual(opponent.get_health(), 90)
+
+        self.unit.attack(opponent)
+        self.assertEqual(opponent.get_health(), 80)
+
+    def test_is_dead_true(self):
+        self.unit.receive_dmg(self.unit.get_health())
+        self.assertTrue(self.unit.is_dead())
+
+        self.unit.receive_dmg(1)
+        self.assertTrue(self.unit.is_dead())
+
+    def test_is_dead_false(self):
+        self.assertFalse(self.unit.is_dead())
+
+        self.unit.receive_dmg(self.unit.get_health() - 1)
+        self.assertFalse(self.unit.is_dead())
+
+
 
 
 
